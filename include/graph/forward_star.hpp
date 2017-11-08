8b495e7a33a623239df6728051bc7a0069fddf02
Moritz Kobitzsch <12932279+MoKob@users.noreply.github.com>
2017-11-08 10:10:06 +0100

    node-iterators to ID ranges, test coverage

--- include/graph/forward_star.hpp
+++ include/graph/forward_star.hpp
@@ -2,8 +2,12 @@
 #define PROJECT_X_GRAPH_FORWARD_STAR_HPP_
 
 #include <boost/range/iterator_range.hpp>
+
 #include <cstdint>
+#include <utility>
 #include <vector>
+
+#include "iterator/pointer.hpp"
 
 namespace project_x {
 namespace graph {
@@ -13,8 +17,9 @@ class ForwardStarGraph {
 public:
   // defines for nodes
   using offset_storage = std::vector<std::uint64_t>;
-  using node_iterator = offset_storage::iterator;
-  using const_node_iterator = offset_storage::const_iterator;
+  using node_iterator = iterator::Pointer<std::uint64_t>;
+  using offset_ptr = std::uint64_t *;
+  using const_node_iterator = iterator::Pointer<std::uint64_t const>;
   using node_range = boost::iterator_range<node_iterator>;
   using const_node_range = boost::iterator_range<const_node_iterator>;
 
@@ -41,43 +46,53 @@ public:
   node_iterator node_end();
   const_node_iterator node_end() const;
 
-  node_iterator node(std::uint64_t const);
-  const_node_iterator node(std::uint64_t const) const;
-
   node_range nodes();
   const_node_range nodes() const;
 
   // access into the edges
   edge_iterator edges_begin();
   const_edge_iterator edges_begin() const;
-  edge_iterator edges_begin(const_node_iterator const);
-  const_edge_iterator edges_begin(const_node_iterator const) const;
+
   edge_iterator edges_begin(std::uint64_t const);
   const_edge_iterator edges_begin(std::uint64_t const) const;
+  const_edge_iterator edges_begin(const_node_iterator const) const;
+  edge_iterator edges_begin(node_iterator const);
+  edge_iterator edges_begin(offset_ptr const);
 
-  edge_iterator edges_end(const_node_iterator const);
-  const_edge_iterator edges_end(const_node_iterator const) const;
   edge_iterator edges_end(std::uint64_t const);
   const_edge_iterator edges_end(std::uint64_t const) const;
+  const_edge_iterator edges_end(const_node_iterator const) const;
+  edge_iterator edges_end(node_iterator const);
+  edge_iterator edges_end(offset_ptr const);
   edge_iterator edges_end();
   const_edge_iterator edges_end() const;
+  edge_iterator edge(std::uint64_t const);
+  const_edge_iterator edge(std::uint64_t const) const;
+
+  edge_iterator edge(node_iterator const);
+  edge_iterator edge(offset_ptr const);
+  const_edge_iterator edge(offset_ptr const) const;
 
   edge_range edges();
   const_edge_range edges() const;
-  edge_range edges(const_node_iterator const);
   edge_range edges(std::uint64_t const);
-  const_edge_range edges(const_node_iterator const) const;
   const_edge_range edges(std::uint64_t const) const;
+  const_edge_range edges(const_node_iterator const) const;
+  edge_range edges(node_iterator const);
+  edge_range edges(offset_ptr const);
 
   // translating into IDs
   std::uint64_t node_id(const_node_iterator const) const;
+  std::uint64_t node_id(node_iterator const) const;
+  std::uint64_t node_id(offset_ptr const) const;
   std::uint64_t edge_id(const_edge_iterator const) const;
+  std::uint64_t edge_id(edge_iterator const) const;
 
 private:
   // cannot construct without an appropriate factory
   ForwardStarGraph();
 
-  std::vector<std::size_t> node_offsets;
+  offset_storage node_offsets;
   storage_type edge_storage;
 
   // ensure that the respective factory is allowed to acces the graph
--- include/graph/forward_star_factory.hpp
+++ include/graph/forward_star_factory.hpp
@@ -5,6 +5,7 @@
 
 #include <algorithm>
 #include <cstdint>
+#include <exception>
 
 namespace project_x {
 namespace graph {
@@ -26,43 +27,64 @@ public:
   template <typename container, typename extractor_type>
   static ForwardStarGraph
   produce_directed_from_edges(std::uint64_t const number_of_nodes,
-                              container &edges, extractor_type extractor) {
-    auto const by_source = [&extractor](auto const &lhs, auto const &rhs) {
-      return extractor.source(lhs) < extractor.source(rhs);
-    };
+                              container &edges, extractor_type extractor);
 
-    // sort edges, so that they are grouped by source
-    std::sort(edges.begin(), edges.end(), by_source);
-
-    ForwardStarGraph graph;
-    graph.node_offsets.reserve(number_of_nodes + 1);
-    graph.node_offsets.push_back(0);
-    graph.edge_storage.reserve(edges.size());
-
-    auto edge = edges.begin();
-    for (std::uint64_t node = 0; node < number_of_nodes; ++node) {
-      // add all edges to the graph
-      while (extractor.source(*edge) == node) {
-        graph.edge_storage.push_back(extractor.target(*edge));
-        ++edge;
-      }
-      // mark the end of the current edges
-      graph.node_offsets.push_back(graph.edge_storage.size());
-    }
-
-    return graph;
-  }
   // helper to allow construction with default extractor
   template <typename container>
   static ForwardStarGraph
   produce_directed_from_edges(std::uint64_t const number_of_nodes,
-                              container &edges) {
-    return produce_directed_from_edges(
-        number_of_nodes, edges,
-        details::SourceTargetExtractor<typename container::value_type>());
-  }
+                              container &edges);
 };
+
+template <typename container, typename extractor_type>
+ForwardStarGraph ForwardStarGraphFactory::produce_directed_from_edges(
+    std::uint64_t const number_of_nodes, container &edges,
+    extractor_type extractor) {
+  auto const by_source = [&extractor](auto const &lhs, auto const &rhs) {
+    return extractor.source(lhs) < extractor.source(rhs);
+  };
+
+  // sort edges, so that they are grouped by source
+  std::stable_sort(edges.begin(), edges.end(), by_source);
+
+  ForwardStarGraph graph;
+  graph.node_offsets.reserve(number_of_nodes + 1);
+  graph.node_offsets.push_back(0);
+  graph.edge_storage.reserve(edges.size());
+
+  auto edge = edges.begin();
+  for (std::uint64_t node = 0; node < number_of_nodes; ++node) {
+    // add all edges to the graph
+    while (extractor.source(*edge) == node) {
+      auto target = extractor.target(*edge);
+      if (target >= number_of_nodes)
+        throw std::out_of_range{
+            "Node: " + std::to_string(target) +
+            " is out of range. Number of nodes was specified as " +
+            std::to_string(number_of_nodes)};
+      graph.edge_storage.push_back(target);
+      ++edge;
+    }
+    // mark the end of the current edges
+    graph.node_offsets.push_back(graph.edge_storage.size());
+  }
+  if (edge != edges.end())
+    throw std::out_of_range(
+        "Some source vertices (" + std::to_string(extractor.target(*edge)) +
+        " and up) are larger than the specified number of nodes.");
+
+  return graph;
 }
+// helper to allow construction with default extractor
+template <typename container>
+ForwardStarGraph ForwardStarGraphFactory::produce_directed_from_edges(
+    std::uint64_t const number_of_nodes, container &edges) {
+  return produce_directed_from_edges(
+      number_of_nodes, edges,
+      details::SourceTargetExtractor<typename container::value_type>());
+}
+
+} // namespace graph
 } // namespace project_x
 
 #endif // PROJECT_X_GRAPH_FORWARD_STAR_FACTORY_HPP_
