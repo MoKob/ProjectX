7c2ff6e9584f4c6d4691ec8756075141c13b62f9
Moritz Kobitzsch <12932279+MoKob@users.noreply.github.com>
2017-11-13 10:30:47 +0100

    creating forward star graphs from OSM using osmium

--- README.md
+++ README.md
@@ -13,3 +13,11 @@ Dependencies:
 apt-get install libboost-all-dev
 ```
 
+## Building a Graph from OSM
+
+Dependencies:
+```
+sudo apt-get install build-essential libboost-python-dev \
+                     libexpat1-dev zlib1g-dev libbz2-dev
+pip3 install osmium
+```
--- src/binding/xpython.cpp
+++ src/binding/xpython.cpp
@@ -8,8 +8,10 @@ class ScopeImport {};
 
 BOOST_PYTHON_MODULE(xpython) {
   using namespace boost::python;
-  scope import = class_<ScopeImport>("import");
+  scope import = class_<ScopeImport>("importer");
 
-  class_<project_x::builder::Graph>("GrapBuilder")
-      .def("add_edge", &project_x::builder::Graph::add_edge);
+  class_<project_x::builder::Graph>("GraphBuilder")
+      .def("add_edge", &project_x::builder::Graph::add_edge)
+      .def("build_graph_and_store",
+           &project_x::builder::Graph::build_graph_and_store);
 }
